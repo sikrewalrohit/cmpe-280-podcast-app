25a0b0bf6c6f184d37461957ef5a295efb483f69
rohit_sikrewal <sikrewalrohit96@gmail.com>
2022-05-12 10:57:23 -0700

    reverted

--- src/hooks/index.js
+++
@@ -1,7 +0,0 @@
-import useAuth from "./useAuth";
-import useDialog from "./useDialog";
-import useForm from "./useForm";
-import useStorage from "./useStorage";
-import useToggle from "./useToggle";
-
-export { useAuth, useDialog, useForm, useStorage, useToggle };
